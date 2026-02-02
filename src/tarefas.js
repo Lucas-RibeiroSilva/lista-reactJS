8806cf3cac42095fe6a46a92cb023686fe274c2e
Lucas-RibeiroSilva <lucascurso0711@gmail.com>
2026-02-02 14:00:32 -0300

    Add files via upload

---
+++ src/tarefas.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {FontAwesome} from '@expo/vector-icons'; 
+
+export default function Tarefa ({data, deleteItem}){
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={deleteItem}>
+            <FontAwesome name="trash" size={24} color="black" />
+            </TouchableOpacity>
+
+            <Text>{data.item}</Text>
+
+
+        </View>
+
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'rgba(196,196,196,0.20)',
+        marginTop: 12,
+        padding: 12, 
+        borderRadius: 4,
+        flexDirection: 'row',
+        borderColor:'black',
+        borderWidth:2
+    },
+    button:{
+        marginRight:8,
+    }
+})
